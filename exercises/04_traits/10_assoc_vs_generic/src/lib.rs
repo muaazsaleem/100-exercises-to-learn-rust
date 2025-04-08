a746603f3546daa20d601d9e4efbb5d5b471ad2f
Muaaz Saleem <muaazsaleem@outlook.com>
2025-04-08 22:18:05 +0200

    Solutions for 9-14
    TODO: use generics to solve 9-14

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,29 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+trait Power<T> {
+    fn power(&self, n: T) -> u32;
+}
+
+impl Power<u16> for u32 {
+    fn power(&self, n: u16) -> u32 {
+        let n: u32 = n.into();
+        self.pow(n)
+    }
+}
+
+impl Power<u32> for u32 {
+    fn power(&self, n: u32) -> u32 {
+        self.pow(n)
+    }
+}
+
+impl Power<&u32> for u32  {
+    fn power(&self, n: &u32) -> u32 {
+        self.pow(*n)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
@@ -33,5 +56,12 @@ mod tests {
     fn test_power_ref_u32() {
         let x: u32 = 2_u32.power(&3u32);
         assert_eq!(x, 8);
+    }
+
+    #[test]
+    fn test_power_of_ref_u32() {
+        let n: u32 = 2;
+        let x: u32 = (&n).power(&3u32);
+        assert_eq!(x, 8)
     }
 }
--- exercises/04_traits/14_outro/tests/integration.rs
+++ exercises/04_traits/14_outro/tests/integration.rs
@@ -1,4 +1,92 @@
-use outro_03::SaturatingU16;
+use std::ops::Add;
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+struct SaturatingU16 {
+    value: u16
+}
+
+impl From<u16> for SaturatingU16 {
+    fn from(value: u16) -> Self {
+        SaturatingU16{
+            value
+        }
+    }
+}
+
+impl From<&u16> for SaturatingU16 {
+    fn from(value: &u16) -> Self {
+        SaturatingU16{
+            value: *value
+        }
+    }
+}
+
+impl From<u8> for SaturatingU16 {
+    fn from(value: u8) -> Self {
+        SaturatingU16{
+            value: value.into()
+        }
+    }
+}
+
+impl From<&u8> for SaturatingU16 {
+    fn from(value: &u8) -> Self {
+        SaturatingU16{
+            value: (*value).into()
+        }
+    }
+}
+
+impl Add<SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: SaturatingU16) -> Self::Output {
+        SaturatingU16{
+            value: self.value.saturating_add(rhs.value)
+        }
+
+    }
+}
+
+impl Add<&SaturatingU16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &SaturatingU16) -> Self::Output {
+        SaturatingU16{
+            value: self.value.saturating_add(rhs.value)
+        }
+
+    }
+}
+
+impl Add<u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: u16) -> Self::Output {
+       SaturatingU16{
+           value: self.value.saturating_add(rhs.into())
+       }
+
+    }
+}
+
+impl Add<&u16> for SaturatingU16 {
+    type Output = SaturatingU16;
+
+    fn add(self, rhs: &u16) -> Self::Output {
+        SaturatingU16{
+            value: self.value.saturating_add((*rhs).into())
+        }
+
+    }
+}
+
+impl PartialEq<u16> for SaturatingU16 {
+    fn eq(&self, other: &u16) -> bool {
+       self.value == *other
+    }
+}
+
 
 #[test]
 fn test_saturating_u16() {
