23549cf571f9f6acdfa69df886301026918fe396
Muaaz Saleem <muaazsaleem@outlook.com>
2026-01-27 22:58:52 +0100

    Solve 06_14

--- exercises/06_ticket_management/14_index_mut/src/lib.rs
+++ exercises/06_ticket_management/14_index_mut/src/lib.rs
@@ -1,6 +1,6 @@
 // TODO: Implement `IndexMut<&TicketId>` and `IndexMut<TicketId>` for `TicketStore`.
 
-use std::ops::Index;
+use std::ops::{Index, IndexMut};
 use ticket_fields::{TicketDescription, TicketTitle};
 
 #[derive(Clone)]
@@ -72,6 +72,21 @@ impl Index<&TicketId> for TicketStore {
 
     fn index(&self, index: &TicketId) -> &Self::Output {
         &self[*index]
+    }
+}
+
+impl IndexMut<TicketId> for TicketStore {
+    fn index_mut(&mut self, index: TicketId) -> &mut Self::Output {
+        self.tickets
+            .iter_mut()
+            .find(|t| t.id == index)
+            .unwrap_or_else(|| panic!("ticket {} not found", index.0))
+    }
+}
+
+impl IndexMut<&TicketId> for TicketStore {
+    fn index_mut(&mut self, index: &TicketId) -> &mut Self::Output {
+        &mut self[*index]
     }
 }
 
