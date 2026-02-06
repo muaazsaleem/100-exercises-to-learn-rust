8c78d0c1ccaaf6434178cce4102533509835434d
Muaaz Saleem <muaazsaleem@outlook.com>
2026-02-06 09:50:57 +0100

    Solve 07_02 static lifetimes

--- exercises/07_threads/02_static/src/lib.rs
+++ exercises/07_threads/02_static/src/lib.rs
@@ -4,7 +4,11 @@
 use std::thread;
 
 pub fn sum(slice: &'static [i32]) -> i32 {
-    todo!()
+    let s1 = thread::spawn(|| slice[..slice.len() / 2].iter().sum::<i32>());
+
+    let s2 = thread::spawn(|| slice[slice.len() / 2..].iter().sum::<i32>());
+
+    s1.join().unwrap() + s2.join().unwrap()
 }
 
 #[cfg(test)]
