34de45e6187ef5f50b1f19109541caca6990f5be
Muaaz Saleem <muaazsaleem@outlook.com>
2026-02-11 20:51:57 +0100

    Solve 07_08 Client

--- exercises/07_threads/08_client/src/lib.rs
+++ exercises/07_threads/08_client/src/lib.rs
@@ -7,23 +7,41 @@ pub mod store;
 
 #[derive(Clone)]
 // TODO: flesh out the client implementation.
-pub struct TicketStoreClient {}
+pub struct TicketStoreClient {
+    sender: Sender<Command>,
+}
 
 impl TicketStoreClient {
     // Feel free to panic on all errors, for simplicity.
     pub fn insert(&self, draft: TicketDraft) -> TicketId {
-        todo!()
+        let (response_sender, response_receiver) = std::sync::mpsc::channel();
+        let command = Command::Insert {
+            draft,
+            response_channel: response_sender,
+        };
+        self.sender.send(command).expect("Oops, couldn't insert!");
+        response_receiver
+            .recv()
+            .expect("Oops, didn't get a TicketId!")
     }
 
     pub fn get(&self, id: TicketId) -> Option<Ticket> {
-        todo!()
+        let (response_sender, response_receiver) = std::sync::mpsc::channel();
+        let command = Command::Get {
+            id,
+            response_channel: response_sender,
+        };
+        self.sender.send(command).expect("Ooops, couldn't get!");
+        response_receiver
+            .recv()
+            .expect("Oops, didn't get a Option<Ticket>")
     }
 }
 
 pub fn launch() -> TicketStoreClient {
     let (sender, receiver) = std::sync::mpsc::channel();
     std::thread::spawn(move || server(receiver));
-    todo!()
+    TicketStoreClient { sender: sender }
 }
 
 // No longer public! This becomes an internal detail of the library now.
